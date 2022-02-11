41fe508dc5132c32652af852516fbf5eb2984497
Allana Karla <allanakarla1702@gmail.com>
2022-02-11 12:14:08 -0300

    Requisito 7 completo

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -2,6 +2,8 @@ import React from 'react';
 import propTypes from 'prop-types';
 import Header from './Header';
 import getMusics from '../services/musicsAPI';
+import MusicCard from './MusicCard';
+import Loading from './Loading';
 
 class Album extends React.Component {
   constructor() {
@@ -12,26 +14,36 @@ class Album extends React.Component {
   }
 
   async componentDidMount() {
-    const { match: { params: { id } } } = this.props;
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    const listMusic = await getMusics(id);
     this.setState({
-      musics: await getMusics(id),
+      musics: listMusic,
     });
   }
 
   render() {
     const { musics } = this.state;
+    if (musics.length === 0) return <Loading />;
     const { artworkUrl100, collectionName, artistName } = musics[0];
     return (
       <div data-testid="page-album">
         <Header />
         <h1>Album</h1>
-        {musics.length > 0
-        && <div>
+        <div>
           <img src={ artworkUrl100 } alt={ collectionName } />
-          <span data-testid="artist-name">{ collectionName }</span>
-          <span data-testid="album-name">{ artistName }</span>
+          <span data-testid="artist-name">{ artistName }</span>
+          <span data-testid="album-name">{ collectionName }</span>
         </div>
-        }
+        <div>
+          { musics.filter(({ kind }) => kind === 'song')
+            .map((song) => (
+              <MusicCard
+                key={ song.trackId }
+                song={ song }
+              />))}
+        </div>
       </div>
     );
   }
